f0229112d6f2c8a4d67101279034d02270087e27
vina-create <tippasani.vinay2023@vitstudent.ac.in>
2026-03-09 19:21:30 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,30 @@
 package com.example;
 
 public class App {
+
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Calculator is running.");
+
+        App calc = new App();
+
+        System.out.println("Addition: " + calc.add(10,5));
+        System.out.println("Subtraction: " + calc.subtract(10,5));
+        System.out.println("Multiplication: " + calc.multiply(10,5));
+        System.out.println("Division: " + calc.divide(10,5));
     }
 }
