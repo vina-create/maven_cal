f7e1be8f8c08a761fec00298060f1daa39d25144
vina-create <tippasani.vinay2023@vitstudent.ac.in>
2026-03-09 19:21:57 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,11 +1,29 @@
 package com.example;
+
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class AppTest {
+
+    App calc = new App();
+
     @Test
     public void testAdd() {
-        App calc = new App();
-        assertEquals(10, calc.add(5, 5));
+        assertEquals(15, calc.add(10,5));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(5, calc.subtract(10,5));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(50, calc.multiply(10,5));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, calc.divide(10,5));
     }
 }
