3561402a5006cd1dcd38ed73b40804fa49a981e9
vina-create <tippasani.vinay2023@vitstudent.ac.in>
2026-03-09 10:30:39 +0530

    Add App class with addition method and main method

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,1 +1,10 @@
+package com.example;
 
+public class App {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        System.out.println("Calculator is running.");
+    }
+}
